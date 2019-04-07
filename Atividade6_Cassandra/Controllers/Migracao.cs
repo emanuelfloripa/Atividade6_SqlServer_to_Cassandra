a3b787948cbd13e2b0096fd74f7a6f3e5c42f5eb
Emanuel Espindola <emanuelx@msn.com>
2019-04-07 17:27:16 -0300

    início. já fazendo conexão load sql

---
+++ Atividade6_Cassandra/Controllers/Migracao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Atividade6_Cassandra.Controllers
+{
+    public class Migracao
+    {
+        private string sql;
+
+        public Migracao()
+        {
+
+        }
+
+        public void LoadSQL()
+        {
+
+
+            string connStr = "";
+            var con = new SqlConnection(connStr);
+            con.Open();
+
+            var command = new SqlCommand(sql, con);
+            var adapter = new SqlDataAdapter();
+
+            try
+            {
+                adapter.InsertCommand = command;
+                adapter.InsertCommand.ExecuteNonQuery();
+
+
+            }
+            finally 
+            {
+                command.Dispose();
+                con.Close();
+            }
+
+
+
+        }
+
+    
+    }
+}
